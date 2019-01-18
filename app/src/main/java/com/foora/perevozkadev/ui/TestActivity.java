94c8ad4b308abd5a83fd190b0c1a70239215e094
Alexandr <alex____98@hotmail.com>
2019-01-18 06:48:42 +0200

    some changes

--- app/src/main/java/com/foora/perevozkadev/ui/TestActivity.java
+++ app/src/main/java/com/foora/perevozkadev/ui/TestActivity.java
@@ -1,21 +1,25 @@
 package com.foora.perevozkadev.ui;
 
-import android.Manifest;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.AutoCompleteTextView;
 
 import com.foora.foora.perevozkadev.R;
+import com.foora.perevozkadev.utils.custom.places.CustomAutoCompleteAdapter;
 
 public class TestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActivityCompat.requestPermissions(TestActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+//        ActivityCompat.requestPermissions(TestActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
 
         setContentView(R.layout.activity_test2);
 
+        AutoCompleteTextView autoCompleteTextView = findViewById(R.id.auto_complete_txtv);
+        autoCompleteTextView.setThreshold(2);
+        CustomAutoCompleteAdapter adapter =  new CustomAutoCompleteAdapter(this);
+        autoCompleteTextView.setAdapter(adapter);
 
     }
 }
